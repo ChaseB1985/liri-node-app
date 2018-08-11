4a1fbda390b0458e16b33f23ab2dc67c194d8039
Chase Barrier <chasebarrier85@gmail.com>
2018-08-10 19:51:05 -0500

    final commit

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log("this is working");
+
 
 exports.twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
--- liri.js
+++ liri.js
@@ -1,81 +1,196 @@
 
-require("dotenv").config();
+var fs = require('fs'); 
+var request = require('request');
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var weather = require('openweather-apis');
 
-var keys = require ('./keys.js');
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
-var action = process.argv[2];
-var value = process.argv[3]; 
-var fs = require('fs');
-//var Spotify = require('node-spotify-api');
+// Grabs the command from the terminal
+var command = process.argv[2];
+var searchValue = "";
 
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-var request = require("request");
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
+// Puts together the search value into one string
+for (var i = 3; i < process.argv.length; i++) {
+    searchValue += process.argv[i] + " ";
+};
 
-// Create an empty variable for holding the movie name
-var movieName = "";
+// Error Functions 
+function errorFunction(respError) {
+    if (respError) {
+        return console.log("Error occured: ", respError);
+     }
+};
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
 
-  if (i > 2 && i < nodeArgs.length) {
 
-    movieName = movieName + "+" + nodeArgs[i];
+// Weather weather-that
+function weatherThat(){
+var apiKey = 'e1e2c44cf769391c740f405b12b64419';
+var city = 'dallas';
+var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-  }
-
-  else {
-
-    movieName += nodeArgs[i];
-
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 }
+)};
+//end of weather function
 
-var movieThis = function(movieValue){
-  if(movieValue === undefined) {
-		movieValue = "mr nobody"};
+// Spotify spotify-this-song 
+function searchSong(searchValue) {
 
-};//end of movie var function
-
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-request(queryUrl, function(error, response, body) {
-
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Title of Film: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country Filmed: " + JSON.parse(body).Country);
-    console.log("Language of Movie: " + JSON.parse(body).Language);
-    console.log("What the Movie is about: " + JSON.parse(body).Plot);
-    console.log("Who was in it: " + JSON.parse(body).Actors);
-    //Rotten Tomatoes
-    for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-      if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-        console.log("* Rotten Tomatoes Rating:     " + JSON.parse(body).Ratings[i].Value);
-        if(JSON.parse(body).Ratings[i].Website !== undefined) {
-          console.log("* Rotten Tomatoes URL:        " + JSON.parse(body).Ratings[i].Website);
-
-        }
-      }
+    // Default search value if no song is given
+    if (searchValue == "") {
+        searchValue = "The Sign Ace of Base";
     }
-  }
-});
-if(action === "my-tweets"){
-  myTweets();
-  } else if(action === "spotify-this-song"){
-    Spotify(); 
-  }else if(action === "movie-this"){
-    movieThis(value); 
-  }else if (action === "do-what-it-says");
+
+    // Accesses Spotify keys  
+    var spotify = new Spotify(keys.spotify);
+
+    var searchLimit = "";
+
+    // Allows the user to input the number of returned spotify results, defaults 1 return if no input given
+    if (isNaN(parseInt(process.argv[3])) == false) {
+        searchLimit = process.argv[3];
+
+        console.log("\nYou requested to return: " + searchLimit + " songs");
+        
+        // Resets the searchValue to account for searchLimit
+        searchValue = "";
+        for (var i = 4; i < process.argv.length; i++) {        
+            searchValue += process.argv[i] + " ";
+        };
+
+    } else {
+        console.log("error try again!")
+        searchLimit = 1;
+    }
+   
+    // Searches Spotify with given values
+    spotify.search({ type: 'track', query: searchValue, limit: searchLimit }, function(respError, response) {
+
+        fs.appendFile("log.txt", "-----Spotify Log Entry Start-----\nProcessed on:\n" + Date()
+         + "\n\n" + "terminal commands:\n" + process.argv + "\n\n" + "Data Output: \n", errorFunctionStart());
+
+        errorFunction();
+
+        var songResp = response.tracks.items;
+
+        for (var i = 0; i < songResp.length; i++) {
+            console.log("\n=============== Spotify Search Result "+ (i+1) +" ===============\n");
+            console.log(("Artist: " + songResp[i].artists[0].name));
+            console.log(("Song title: " + songResp[i].name));
+            console.log(("Album name: " + songResp[i].album.name));
+            console.log(("URL Preview: " + songResp[i].preview_url));
+            console.log("\n=========================================================\n");
+
+            fs.appendFile("log.txt", "\n========= Result "+ (i+1) +" =========\nArtist: "
+             + songResp[i].artists[0].name + "\nSong title: " + songResp[i].name + "\nAlbum name: " + songResp[i].album.name + "\nURL Preview: "
+             + songResp[i].preview_url + "\n=============================\n", errorFunction());
+        }
+
+        fs.appendFile("log.txt","-----Spotify Log Entry End-----\n\n", errorFunctionEnd());
+    })
+};
+
+// OMDB movie-this 
+function searchMovie(searchValue) {
+
+    // Default search value if no movie is given
+    if (searchValue == "") {
+        searchValue = "Mr. Nobody";
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchValue.trim() + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function(respError, response, body) {
+
+        fs.appendFile("log.txt", "-----OMDB Log Entry Start-----\n\nProcessed on:\n" + Date()
+         + "\n\n" + "terminal commands:\n" + process.argv + "\n\n" + "Data Output: \n\n", errorFunctionStart());
+
+        errorFunction();
+
+        if (JSON.parse(body).Error == 'Movie not found!' ) {
+
+            console.log("\nI'm sorry, I could not find any movies that matched the title " + searchValue + ". Please check your spelling and try again.\n")
+
+            fs.appendFile("log.txt", "I'm sorry, I could not find any movies that matched the title " + searchValue + ". Please check your spelling and try again.\n\n-----OMDB Log Entry End-----\n\n", errorFunctionEnd());
+        
+        } else {
+
+            movieBody = JSON.parse(body);
+
+            console.log("\n++++++++++++++++ OMDB Search Results ++++++++++++++++\n");
+            console.log("Movie Title: " + movieBody.Title);
+            console.log("Year: " + movieBody.Year);
+            console.log("IMDB rating: " + movieBody.imdbRating);
+
+            // If there is no Rotten Tomatoes Rating
+            if (movieBody.Ratings.length < 2) {
+
+                console.log("There is no Rotten Tomatoes Rating for this movie.")
+
+                fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: There is no Rotten Tomatoes Rating for this movie \nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+                
+            } else {
+
+                console.log("Rotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value);
+
+                fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value + "\nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+            }
+            
+            console.log("Country: " + movieBody.Country);
+            console.log("Language: " + movieBody.Language);
+            console.log("Plot: " + movieBody.Plot);
+            console.log("Actors: " + movieBody.Actors);
+            console.log("\n+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+            console.log("xxxx Log Ended xxxx");
+        };      
+    });
+};
+
+// Random do-what-it-says 
+function randomSearch() {
+
+    fs.readFile("random.txt", "utf8", function(respError, data) {
+
+        var randomArray = data.split(", ");
+
+        errorFunction();
+
+        if (randomArray[0] == "spotify-this-song") {
+            searchSong(randomArray[1]);
+        } else if (randomArray[0] == "movie-this") {
+            searchMovie(randomArray[1]);
+        } else {
+            weatherThat();
+        }
+    });
+};
+
+// Switch Case 
+
+
+// Runs corresponding function based on user command
+switch (command) {
+    case "weather-that":
+        weatherThat();
+        break;
+    case "spotify-this-song":
+        searchSong(searchValue);
+        break;
+    case "movie-this":
+        searchMovie(searchValue);
+        break;
+    case "do-what-it-says":
+        randomSearch();
+        break;
+    default:
+        console.log("Error try again");
+};
